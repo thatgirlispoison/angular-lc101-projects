f557cc57cffe7a1348d5fb0489a2eaf305d15071
Kelly Thomas <kellydye77@gmail.com>
2020-10-29 14:32:57 -0500

    Part 1 Exercise of Lesson 1

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
-  image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  photosTitle = 'Here are my favorite photos!';
+  image1 = 'https://rlv.zcache.com/lazer_warrior_space_cat_riding_panda_with_taco_poster-r4cba1dce598f479baf42d00d74c2e9c0_a7240_8byvr_704.jpg';
+  image2 = 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSchtbX-ezHOUJXXJU1hi3OyABPzsNcOPQXzA&usqp=CAU';
+  image3 = 'https://images.fineartamerica.com/images/artworkimages/medium/1/3-laser-eyes-space-cat-riding-sloth-dog-rainbow-random-galaxy.jpg';
 
   constructor() { }
 
