a87be3b9f887e78c9e2025ee4e5e9cbdd26ee70a
Kelly Thomas <kellydye77@gmail.com>
2020-10-31 13:36:55 -0500

    Finished ngIf practice

--- lesson2/examples/ngif-practice/src/app/chores/chores.component.ts
+++ lesson2/examples/ngif-practice/src/app/chores/chores.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChoresComponent implements OnInit {
    chores = [];
-   finishedChores = [];
+   finishedChores = ['Wash Dishes', 'Do Laundry'];
 
    targetImage = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
 
@@ -15,8 +15,6 @@ export class ChoresComponent implements OnInit {
 
    ngOnInit() {
    }
-
-}
  
-// If the chores array is empty OR the finishedChores array has at least 3 more items than the chores array, 
-// display targetImage under the Done Chores list.Otherwise, use a p tag to display the text, "No allowance yet."
+}
+
