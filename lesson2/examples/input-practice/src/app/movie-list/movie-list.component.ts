dd4c4d31cbc0fbcfd9d7d6b7541a0cfc0c23c8df
Kelly Thomas <kellydye77@gmail.com>
2020-10-31 15:08:14 -0500

    saving

--- lesson2/examples/input-practice/src/app/movie-list/movie-list.component.ts
+++ lesson2/examples/input-practice/src/app/movie-list/movie-list.component.ts
@@ -13,11 +13,14 @@ export class MovieListComponent implements OnInit {
    ngOnInit() {
    }
    addMovie(newTitle: string) {
+      let errorMsg = '';
       if (newTitle === '') {
-         alert('You cannot enter an empty movie!')
-      }
-      else if (!this.movies.includes(newTitle)) {
+         errorMsg = 'Please enter a movie title.';
+      } else if (this.movies.includes(newTitle)) {
+         errorMsg = `${newTitle} is already in your movie list.`;
+      } else {
          this.movies.push(newTitle);
-      } 
+      }
+      return errorMsg;
    }
 }
